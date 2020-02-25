a5ca602fdbd1adbbd5c8b8cfd9037e6b62172cb0
Ed Goad <edgoad@gmail.com>
2020-02-25 10:29:31 -0800

    Create 23-ServerDM2.ps1

---
+++ 23-ServerDM2.ps1
@@ -0,0 +1,23 @@
+#######################################################################
+#
+# Run on ServerDM2 after additional NICs are setup
+#
+#######################################################################
+
+# Rename NICs 
+Rename-NetAdapter -Name "Ethernet 2" -NewName Private 
+
+# Set UP addresses 
+New-NetIPAddress -InterfaceAlias Private -IPAddress 192.168.1.3 -PrefixLength 24 
+
+
+# Note: These lines are duplicate, but could be beneficial to re-run
+# Configure Power save 
+powercfg -change -monitor-timeout-ac 0 
+
+# IE Enhaced mode 
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}" -Name IsInstalled -Value 0 
+
+# Set UAC 
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value 0 -Type "Dword" 
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "PromptOnSecureDesktop" -Value 0 -Type "Dword" 
