40defc15a16cc4acf774f42e78e4b5ef8381c872
Ed Goad <edgoad@gmail.com>
2020-02-25 12:52:11 -0800

    Updated to use Hyper-V Direct

--- 11-ServerDC1.ps1
+++ 11-ServerDC1.ps1
@@ -1,45 +1,74 @@
 #######################################################################
 #
 # Run on ServerDM1 once OS is installed
+# Updated to run remotely using Hyper-v Direct
+#
 # NOTE: Will require several reboots
 #
 #######################################################################
+
+# Setup credentials
+$user = "administrator"
+$pass = ConvertTo-SecureString "Password01" -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential($user, $pass)
 
 # Configure name 
 #######################################################################
 # NOTE: REBOOT!
 #######################################################################
-Rename-Computer -NewName ServerDC1 -force -restart 
+Invoke-Command -VMName ServerDC1 -Credential $cred -ScriptBlock { 
+    Rename-Computer -NewName ServerDC1 -force -restart 
+    }
 
 # Rename NICs 
-Rename-NetAdapter -Name Ethernet0 -NewName Internal 
+Invoke-Command -VMName ServerDC1 -Credential $cred -ScriptBlock { 
+    Get-NetAdapter | Rename-NetAdapter -NewName Internal 
+    }
 
 # Set UP addresses 
-New-NetIPAddress -InterfaceAlias Internal -IPAddress 192.168.0.1 -PrefixLength 24 -DefaultGateway 192.168.0.250 
-Set-DnsClientServerAddress -InterfaceAlias Internal -ServerAddresses 127.0.0.1 
+Invoke-Command -VMName ServerDC1 -Credential $cred -ScriptBlock {
+    New-NetIPAddress -InterfaceAlias Internal -IPAddress 192.168.0.1 -PrefixLength 24 -DefaultGateway 192.168.0.250 
+    Set-DnsClientServerAddress -InterfaceAlias Internal -ServerAddresses 127.0.0.1 
+    }
 
 # Install ADDS 
-Install-WindowsFeature AD-Domain-Services -IncludeManagementTools 
+Invoke-Command -VMName ServerDC1 -Credential $cred -ScriptBlock {
+    Install-WindowsFeature AD-Domain-Services -IncludeManagementTools 
+    } 
 
 # Configure AD 
 #######################################################################
 # NOTE: REBOOT!
 #######################################################################
-$smPass = ConvertTo-SecureString "Password01" -AsPlainText -Force 
-Install-ADDSForest -DomainName "MCSA2016.local" -SafeModeAdministratorPassword $smPass -Confirm:$false 
+Invoke-Command -VMName ServerDC1 -Credential $cred -ScriptBlock {
+    $smPass = ConvertTo-SecureString "Password01" -AsPlainText -Force 
+    Install-ADDSForest -DomainName "MCSA2016.local" -SafeModeAdministratorPassword $smPass -Confirm:$false 
+    }
 
 
-# Fix DNS (if needed) 
-Get-DnsServerForwarder | Remove-DnsServerForwarder -Force 
+# Update credentials for AD domain
+$userDom = "mcsa2016\administrator"
+$passDom = ConvertTo-SecureString "Password01" -AsPlainText -Force
+$credDom = New-Object System.Management.Automation.PSCredential($userDom, $pass)
 
-# Configure Power save 
-powercfg -change -monitor-timeout-ac 0 
+    # Fix DNS (if needed) 
+Invoke-Command -VMName ServerDC1 -Credential $credDom -ScriptBlock {
+    Get-DnsServerForwarder | Remove-DnsServerForwarder -Force 
+    }
 
-# IE Enhaced mode 
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}" -Name IsInstalled -Value 0 
+    # Configure Power save 
+Invoke-Command -VMName ServerDC1 -Credential $credDom -ScriptBlock {
+    powercfg -change -monitor-timeout-ac 0 
+    }
 
-# Set UAC 
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value 0 -Type "Dword" 
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "PromptOnSecureDesktop" -Value 0 -Type "Dword" 
+    # IE Enhaced mode 
+Invoke-Command -VMName ServerDC1 -Credential $credDom -ScriptBlock {
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}" -Name IsInstalled -Value 0 
+    }
 
+    # Set UAC 
+Invoke-Command -VMName ServerDC1 -Credential $credDom -ScriptBlock {
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value 0 -Type "Dword" 
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "PromptOnSecureDesktop" -Value 0 -Type "Dword" 
+    }
  
