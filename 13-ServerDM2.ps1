d0184e1a761936d0a246f23b82971ccb4036a452
Ed Goad <edgoad@gmail.com>
2020-02-25 14:39:06 -0800

    Fixed IP address

--- 13-ServerDM2.ps1
+++ 13-ServerDM2.ps1
@@ -30,7 +30,7 @@ Invoke-Command -Session $sessionDM2 -ScriptBlock {
 
 # Set UP addresses 
 Invoke-Command -Session $sessionDM2 -ScriptBlock { 
-    New-NetIPAddress -InterfaceAlias Internal -IPAddress 192.168.0.2 -PrefixLength 24 -DefaultGateway 192.168.0.250 
+    New-NetIPAddress -InterfaceAlias Internal -IPAddress 192.168.0.3 -PrefixLength 24 -DefaultGateway 192.168.0.250 
     }
 # Set UP addresses 
 Invoke-Command -Session $sessionDM2 -ScriptBlock { 
