e41029164dc72414e9bac79e203782366df71266
Ed Goad <edgoad@gmail.com>
2020-02-25 14:39:59 -0800

    Configured NICs via PowerShell Direct

--- 20-MainSetup.ps1
+++ 20-MainSetup.ps1
@@ -20,3 +20,36 @@ Add-VMNetworkAdapter -VMName ServerDC1 -SwitchName Private
 Add-VMNetworkAdapter -VMName ServerDM1 -SwitchName Private
 Add-VMNetworkAdapter -VMName ServerDM2 -SwitchName Private
 Add-VMNetworkAdapter -VMName ServerSA1 -SwitchName Private
+
+
+
+# Start VMs
+Get-VM | Start-VM
+
+# Setup credentials
+$user = "administrator"
+$pass = ConvertTo-SecureString "Password01" -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential($user, $pass)
+# Update credentials for AD domain
+$userDom = "mcsa2016\administrator"
+$passDom = ConvertTo-SecureString "Password01" -AsPlainText -Force
+$credDom = New-Object System.Management.Automation.PSCredential($userDom, $pass)
+
+
+# Configure new NIC
+Invoke-Command -VMName ServerDC1 -Credential $credDom -ScriptBlock { 
+    Get-NetAdapter | where Name -NE 'Internal' | Rename-NetAdapter -NewName Private
+    New-NetIPAddress -InterfaceAlias Private -IPAddress 192.168.1.1 -PrefixLength 24 
+    }
+Invoke-Command -VMName ServerDM1 -Credential $credDom -ScriptBlock { 
+    Get-NetAdapter | where Name -NE 'Internal' | Rename-NetAdapter -NewName Private
+    New-NetIPAddress -InterfaceAlias Private -IPAddress 192.168.1.2 -PrefixLength 24 
+    }
+Invoke-Command -VMName ServerDM2 -Credential $cred -ScriptBlock { 
+    Get-NetAdapter | where Name -NE 'Internal' | Rename-NetAdapter -NewName Private
+    New-NetIPAddress -InterfaceAlias Private -IPAddress 192.168.1.3 -PrefixLength 24 
+    }
+Invoke-Command -VMName ServerSA1 -Credential $cred -ScriptBlock { 
+    Get-NetAdapter | where Name -NE 'Internal' | Rename-NetAdapter -NewName Private
+    New-NetIPAddress -InterfaceAlias Private -IPAddress 192.168.1.4 -PrefixLength 24 
+    }
