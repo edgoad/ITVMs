b744a10e19d492386ce73fd1895ed07f70491a36
Ed Goad <edgoad@gmail.com>
2020-03-03 13:48:59 -0800

    Added non-eval license
    Using mixture of DISM and SLMGR to setup AVMA license
    DISM to reset to generic license key (per https://docs.microsoft.com/en-us/windows-server/get-started/kmsclientkeys)
    SLMGR to specify AVMA key (per https://docs.microsoft.com/en-us/windows-server/get-started-19/vm-activation-19)

--- 20-MainSetup.ps1
+++ 20-MainSetup.ps1
@@ -51,3 +51,45 @@ Invoke-Command -VMName ServerSA1 -Credential $cred -ScriptBlock {
     New-NetIPAddress -InterfaceAlias Private -IPAddress 192.168.1.4 -PrefixLength 24 
     }
 #endregion
+
+#region Use DISM to change Server Edition
+#######################################################################
+# NOTE: REBOOT!
+# Will also return an error code - this is expected
+#######################################################################
+Invoke-Command -VMName ServerDC1 -Credential $credDom -ScriptBlock { 
+    dism /online /Set-Edition:ServerDataCenter /AcceptEULA /quiet /ProductKey:CB7KF-BWN84-R7R2Y-793K2-8XDDG
+    }
+Invoke-Command -VMName ServerDM1 -Credential $credDom -ScriptBlock { 
+    dism /online /Set-Edition:ServerDataCenter /AcceptEULA /quiet /ProductKey:CB7KF-BWN84-R7R2Y-793K2-8XDDG
+    }
+Invoke-Command -VMName ServerDM2 -Credential $cred -ScriptBlock { 
+    dism /online /Set-Edition:ServerDataCenter /AcceptEULA /quiet /ProductKey:CB7KF-BWN84-R7R2Y-793K2-8XDDG
+    }
+Invoke-Command -VMName ServerSA1 -Credential $cred -ScriptBlock { 
+    dism /online /Set-Edition:ServerDataCenter /AcceptEULA /quiet /ProductKey:CB7KF-BWN84-R7R2Y-793K2-8XDDG
+    }
+#endregion
+
+#region Use SLMGR to setup AVMA license key
+# Setup credentials
+$user = "administrator"
+$pass = ConvertTo-SecureString "Password01" -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential($user, $pass)
+# Update credentials for AD domain
+$userDom = "mcsa2016\administrator"
+$passDom = ConvertTo-SecureString "Password01" -AsPlainText -Force
+$credDom = New-Object System.Management.Automation.PSCredential($userDom, $pass)
+Invoke-Command -VMName ServerDC1 -Credential $credDom -ScriptBlock { 
+    cscript //B %windir%\system32\slmgr.vbs /ipk TMJ3Y-NTRTM-FJYXT-T22BY-CWG3J
+    }
+Invoke-Command -VMName ServerDM1 -Credential $credDom -ScriptBlock { 
+    cscript //B %windir%\system32\slmgr.vbs /ipk TMJ3Y-NTRTM-FJYXT-T22BY-CWG3J
+    }
+Invoke-Command -VMName ServerDM2 -Credential $cred -ScriptBlock { 
+    cscript //B %windir%\system32\slmgr.vbs /ipk TMJ3Y-NTRTM-FJYXT-T22BY-CWG3J
+    }
+Invoke-Command -VMName ServerSA1 -Credential $cred -ScriptBlock { 
+    cscript //B %windir%\system32\slmgr.vbs /ipk TMJ3Y-NTRTM-FJYXT-T22BY-CWG3J
+    }
+#endregion
