6a604d5155fb7dff15e7139d62bc694fbf02f6cc
Ed Goad <edgoad@gmail.com>
2020-05-01 10:41:27 -0700

    Added Chrome and fixed DNS

--- IT135-Server/11-Win10VM.ps1
+++ IT135-Server/11-Win10VM.ps1
@@ -6,6 +6,11 @@
 #
 #######################################################################
 
+# Setup credentials
+$user = "Student"
+$pass = ConvertTo-SecureString "Password01" -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential($user, $pass)
+
 
 #region Configure OS
 # Setup session (must be done after rebooting)
@@ -20,14 +25,8 @@ Invoke-Command -Session $sessionWin10 -ScriptBlock {
 # Set UP addresses 
 Invoke-Command -Session $sessionWin10 -ScriptBlock { 
     New-NetIPAddress -InterfaceAlias Internal -IPAddress 192.168.0.4 -PrefixLength 24 -DefaultGateway 192.168.0.250 
-    Set-DnsClientServerAddress -InterfaceAlias Internal -ServerAddresses 192.168.0.1
+    Set-DnsClientServerAddress -InterfaceAlias Internal -ServerAddresses 8.8.8.8
     }
-
-# Configure Power save 
-Invoke-Command -Session $sessionWin10 -ScriptBlock { 
-    powercfg -change -monitor-timeout-ac 0 
-    }
-
 
 # Copy BGInfo
 Copy-Item -ToSession $sessionWin10 -Path "C:\bginfo\" -Destination "C:\bginfo\" -Force -Recurse
@@ -35,13 +34,14 @@ Copy-Item -ToSession $sessionWin10 -Path "C:\bginfo\" -Destination "C:\bginfo\" 
 Invoke-Command -Session $sessionWin10 -ScriptBlock {
     Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name BgInfo -Value "c:\bginfo\bginfo.exe c:\bginfo\default.bgi /timer:0 /silent /nolicprompt"
     }
+
+Invoke-Command -Session $sessionWin10 -ScriptBlock {
+    # install chrome
+    $LocalTempDir = $env:TEMP; $ChromeInstaller = "ChromeInstaller.exe"; (new-object    System.Net.WebClient).DownloadFile('http://dl.google.com/chrome/install/375.126/chrome_installer.exe', "$LocalTempDir\$ChromeInstaller"); & "$LocalTempDir\$ChromeInstaller" /silent /install; $Process2Monitor =  "ChromeInstaller"; Do { $ProcessesFound = Get-Process | ?{$Process2Monitor -contains $_.Name} | Select-Object -ExpandProperty Name; If ($ProcessesFound) { "Still running: $($ProcessesFound -join ', ')" | Write-Host; Start-Sleep -Seconds 2 } else { rm "$LocalTempDir\$ChromeInstaller" -ErrorAction SilentlyContinue -Verbose } } Until (!$ProcessesFound)
+}
 #endregion
 
 #region Rename server
-# Setup credentials
-$user = "administrator"
-$pass = ConvertTo-SecureString "Password01" -AsPlainText -Force
-$cred = New-Object System.Management.Automation.PSCredential($user, $pass)
 
 # Configure name 
 #######################################################################
