54c41db5c0535a639cf44a5c47f4dec8df6c29a4
Ed Goad <edgoad@gmail.com>
2020-05-01 09:04:35 -0700

    Create 20-MainSetup.ps1

---
+++ IT135-Server/20-MainSetup.ps1
@@ -0,0 +1,35 @@
+#######################################################################
+#
+# Second script for building Hyper-V environment for IT 160
+# Run only after the OS is installed and initial configuration complete
+#
+#######################################################################
+
+# Start VMs
+Get-VM | Start-VM
+
+# Setup credentials
+$user = "administrator"
+$pass = ConvertTo-SecureString "Password01" -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential($user, $pass)
+
+#region Use DISM to change Server Edition
+#######################################################################
+# NOTE: REBOOT!
+# Will also return an error code - this is expected
+#######################################################################
+Invoke-Command -VMName Win10VM -Credential $cred -ScriptBlock { 
+    dism /online /Set-Edition:ServerDataCenter /AcceptEULA /quiet /ProductKey:CB7KF-BWN84-R7R2Y-793K2-8XDDG
+    }
+#endregion
+
+#region Use SLMGR to setup AVMA license key
+# Setup credentials
+$user = "administrator"
+$pass = ConvertTo-SecureString "Password01" -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential($user, $pass)
+
+Invoke-Command -VMName Win10VM -Credential $cred -ScriptBlock { 
+    cscript //B %windir%\system32\slmgr.vbs /ipk TMJ3Y-NTRTM-FJYXT-T22BY-CWG3J
+    }
+#endregion
