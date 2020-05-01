551e46b8e99ea6e438b31c5028b2e30d96290095
Ed Goad <edgoad@gmail.com>
2020-05-01 09:11:39 -0700

    Create 11-Win10VM.ps1

---
+++ IT135-Server/11-Win10VM.ps1
@@ -0,0 +1,62 @@
+#######################################################################
+#
+# Run on Win10VM once OS is installed
+# Updated to run remotely using Hyper-v Direct
+# NOTE: Will require several reboots
+#
+#######################################################################
+
+#region Rename server
+# Setup credentials
+$user = "administrator"
+$pass = ConvertTo-SecureString "Password01" -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential($user, $pass)
+
+# Configure name 
+#######################################################################
+# NOTE: REBOOT!
+#######################################################################
+Invoke-Command -VMName Win10VM -Credential $cred -ScriptBlock { 
+    Rename-Computer -NewName Win10VM -force -restart 
+    }
+#endregion
+
+#region Configure OS
+# Setup session (must be done after rebooting)
+$sessionSA1 = New-PSSession -VMName Win10VM -Credential $cred
+
+# Rename NICs 
+Invoke-Command -Session $sessionSA1 -ScriptBlock { 
+    Get-NetAdapter | Rename-NetAdapter -NewName Internal 
+    }
+
+
+# Set UP addresses 
+Invoke-Command -Session $sessionSA1 -ScriptBlock { 
+    New-NetIPAddress -InterfaceAlias Internal -IPAddress 192.168.0.4 -PrefixLength 24 -DefaultGateway 192.168.0.250 
+    Set-DnsClientServerAddress -InterfaceAlias Internal -ServerAddresses 192.168.0.1
+    }
+
+# Configure Power save 
+Invoke-Command -Session $sessionSA1 -ScriptBlock { 
+    powercfg -change -monitor-timeout-ac 0 
+    }
+
+# IE Enhaced mode 
+Invoke-Command -Session $sessionSA1 -ScriptBlock { 
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}" -Name IsInstalled -Value 0 
+    }
+
+# Set UAC 
+Invoke-Command -Session $sessionSA1 -ScriptBlock { 
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value 0 -Type "Dword" 
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "PromptOnSecureDesktop" -Value 0 -Type "Dword" 
+    }
+
+# Copy BGInfo
+Copy-Item -ToSession $sessionSA1 -Path "C:\bginfo\" -Destination "C:\bginfo\" -Force -Recurse
+ # Set autorun
+Invoke-Command -Session $sessionSA1 -ScriptBlock {
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name BgInfo -Value "c:\bginfo\bginfo.exe c:\bginfo\default.bgi /timer:0 /silent /nolicprompt"
+    }
+#endregion
