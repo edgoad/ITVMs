61fa976651f599ee7450dad5a7a5c0e71552820d
Ed Goad <edgoad@gmail.com>
2020-05-01 09:42:49 -0700

    Updated Session name

--- IT135-Server/11-Win10VM.ps1
+++ IT135-Server/11-Win10VM.ps1
@@ -1,6 +1,6 @@
 #######################################################################
 #
-# Run once Win10VM OS is installed
+# Run on Win10VM once OS is installed
 # Updated to run remotely using Hyper-v Direct
 # NOTE: Will require several reboots
 #
@@ -9,30 +9,30 @@
 
 #region Configure OS
 # Setup session (must be done after rebooting)
-$sessionSA1 = New-PSSession -VMName Win10VM -Credential $cred
+$sessionWin10 = New-PSSession -VMName Win10VM -Credential $cred
 
 # Rename NICs 
-Invoke-Command -Session $sessionSA1 -ScriptBlock { 
+Invoke-Command -Session $sessionWin10 -ScriptBlock { 
     Get-NetAdapter | Rename-NetAdapter -NewName Internal 
     }
 
 
 # Set UP addresses 
-Invoke-Command -Session $sessionSA1 -ScriptBlock { 
+Invoke-Command -Session $sessionWin10 -ScriptBlock { 
     New-NetIPAddress -InterfaceAlias Internal -IPAddress 192.168.0.4 -PrefixLength 24 -DefaultGateway 192.168.0.250 
     Set-DnsClientServerAddress -InterfaceAlias Internal -ServerAddresses 192.168.0.1
     }
 
 # Configure Power save 
-Invoke-Command -Session $sessionSA1 -ScriptBlock { 
+Invoke-Command -Session $sessionWin10 -ScriptBlock { 
     powercfg -change -monitor-timeout-ac 0 
     }
 
 
 # Copy BGInfo
-Copy-Item -ToSession $sessionSA1 -Path "C:\bginfo\" -Destination "C:\bginfo\" -Force -Recurse
+Copy-Item -ToSession $sessionWin10 -Path "C:\bginfo\" -Destination "C:\bginfo\" -Force -Recurse
  # Set autorun
-Invoke-Command -Session $sessionSA1 -ScriptBlock {
+Invoke-Command -Session $sessionWin10 -ScriptBlock {
     Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name BgInfo -Value "c:\bginfo\bginfo.exe c:\bginfo\default.bgi /timer:0 /silent /nolicprompt"
     }
 #endregion
