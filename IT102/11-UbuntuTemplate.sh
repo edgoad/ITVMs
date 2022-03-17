c0dbfef3a7d68d7a67808fd2c3a5ff85077da1cf
Ed Goad <edgoad@gmail.com>
2022-03-17 09:11:35 -0700

    Update 11-UbuntuTemplate.sh
    Updated VSCode install steps

--- IT102/11-UbuntuTemplate.sh
+++ IT102/11-UbuntuTemplate.sh
@@ -24,11 +24,14 @@ snap install postman
 
 #install VSCode
 logger -t devvm "Installing VSCode: $?"
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-apt-get update
-apt-get install -y code
+apt-get install wget gpg -y
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+apt install apt-transport-https -y
+apt update
+apt install code -y
 logger -t devvm "VSCode Installed: $?"
 logger -t devvm "Success"
 
